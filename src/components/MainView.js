86bca1bc6ea7757a17417bf52f0c6e109ea24157
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 19:06:14 +0200

    show Cards by default, enable terminals with button in MainView

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -1,9 +1,18 @@
 import React from "react";
 import {CardColumns, Container} from "react-bootstrap";
 import KatacodaCard from "./mainview/KatacodaCard";
+import KatacodaEmbed from "./mainview/KatacodaEmbed";
 
 const MainView = props => {
-    const {embeds, color} = props;
+    const {embeds, buttonHandler, color} = props;
+
+    const isAnyEmbedActive = () => {
+        for(let embed of embeds) {
+            if(embed.isActive)
+                return true;
+        }
+        return false;
+    }
 
     return (
         <Container id={"MainView"}>
@@ -11,8 +20,27 @@ const MainView = props => {
                 <div id={"Embeds"}>
                     <h1>Try our software here!</h1>
                     <CardColumns className={"pt-3"}>
-                        {embeds.map((embed, idx) => <KatacodaCard key={idx} num={idx} embed={embed} color={color} />)}
+                        {embeds.map((embed, idx) =>
+                            <KatacodaCard
+                            key={idx}
+                            num={idx}
+                            embed={embed}
+                            buttonHandler={buttonHandler}
+                            />)}
                     </CardColumns>
+                    {isAnyEmbedActive() &&
+                    <div>
+                        <hr/>
+                        {embeds.filter(e => e.isActive)
+                        .map((embed, idx) =>
+                            <KatacodaEmbed
+                                key={idx}
+                                num={idx}
+                                embed={embed}
+                                color={color}
+                            />)}
+                    </div>
+                    }
                 </div> :
                 <div id={"NoEmbeds"}>
                     <h1>No embeds added to the website. Maybe there's a configuration error?</h1>
