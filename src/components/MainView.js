b3107b847c08d6c731dd7364918578524e6ecfd3
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 15:34:53 +0200

    unpack props

--- src/components/MainView.js
+++ src/components/MainView.js
@@ -3,12 +3,14 @@ import {Container} from "react-bootstrap";
 import KatacodaEmbed from "./mainview/KatacodaEmbed";
 
 const MainView = props => {
+    const {embeds, color} = props;
+
     return (
         <Container id={"MainView"}>
-            {props.embeds ?
+            {embeds ?
                 <div id={"Embeds"}>
                     <h1>Try our software here!</h1>
-                    {props.embeds.map((embed, idx) => <KatacodaEmbed key={idx} num={idx} embed={embed} color={color} />)}
+                    {embeds.map((embed, idx) => <KatacodaEmbed key={idx} num={idx} embed={embed} color={color} />)}
                 </div> :
                 <div id={"NoEmbeds"}>
                     <h1>No embeds added to the website. Maybe there's a configuration error?</h1>
