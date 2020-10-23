c020536ea98ef83c025fb5e697b219c0ad0762bc
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 16:31:43 +0200

    add KatacodaCard component

---
+++ src/components/mainview/KatacodaCard.js
@@ -0,0 +1,41 @@
+import React, {useState} from "react";
+import {Button, Card} from "react-bootstrap";
+import {buttonTexts} from "../../utils";
+
+const KatacodaCard = props => {
+    const {num, embed, color} = props;
+    const [isEmbedActive, setIsEmbedActive] = useState(false);
+    const [buttonText, setButtonText] = useState(buttonTexts.toPress);
+
+    const toggleButton = () => {
+        setButtonText(buttonText === buttonTexts.toPress ? buttonTexts.pressed : buttonTexts.toPress);
+        setIsEmbedActive(!isEmbedActive);
+    }
+
+    return (
+        <Card id={`card-${num}`}
+            border={"success"}
+        >
+            <Card.Img
+                src={`${embed.type}.svg`}
+                alt={embed.title}
+                variant={"top"}
+            />
+            <Card.Body>
+                <Card.Title>{embed.title}</Card.Title>
+                <Card.Text>{embed.description}</Card.Text>
+                <Button
+                    variant={"success"}
+                    onClick={toggleButton}
+                >
+                    {buttonText}
+                </Button>
+            </Card.Body>
+            <Card.Footer>
+                <a href={`https://github.com/${embed.github}`}>GitHub page</a>
+            </Card.Footer>
+        </Card>
+    );
+}
+
+export default KatacodaCard;
