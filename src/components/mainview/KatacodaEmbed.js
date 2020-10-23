ada04f69490b283559aa6b829c3df900e72366dd
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 19:43:12 +0200

    use style instead of conditional rendering in KatacodaEmbed

--- src/components/mainview/KatacodaEmbed.js
+++ src/components/mainview/KatacodaEmbed.js
@@ -4,14 +4,26 @@ const KatacodaEmbed = props => {
     const {num, embed, color} = props;
 
     return (
-        <div id={`embed-${num}`} className={"pt-3"}>
+        <div
+            id={`embed-${num}`}
+            className={"pt-3"}
+            style={{
+                // use this instead of ReactJS conditional rendering
+                // because otherwise Katacoda doesn't contact the backend
+                visibility: embed.isActive ? "visible": "hidden",
+                display: embed.isActive ? "initial" : "none",
+            }}
+        >
+            <hr/>
             <h3>{embed.title}</h3>
             <div id={`katacoda-scenario-${num}`}
                 data-katacoda-id={embed.katacoda}
                 data-katacoda-color={color}
-                style={{height: "600px", paddingTop: "20px"}}
+                style={{
+                    height: "600px",
+                    paddingTop: "20px",
+                }}
             />
-            <hr/>
         </div>
     );
 }
