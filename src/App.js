1b3c4d5add99103ef1096f38dcb854572b6512e0
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 16:28:09 +0200

    fix utils import in App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import embedTypes from "./utils";
+import utils from "./utils";
 import Header from "./components/Header";
 import MainView from "./components/MainView";
 
 const App = () => {
+    const embedTypes = utils.embedTypes;
 
     // state variables
     const [embeds, setEmbeds] = useState([
