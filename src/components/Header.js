58f6e17e657512279e137df14168543a8507e8e1
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 10:38:45 +0200

    add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+// this component is called Header to prevent collisions with react-bootstrap's Navbar component
+
+import React from "react";
+import {Navbar, Container} from "react-bootstrap";
+
+const Header = props => {
+    return(
+        <Navbar
+        expand={"sm"}
+        fixed={"top"}>
+            <Container>
+                <Navbar.Brand
+                href={"/"}>
+                    <img
+                        src={"weee.png"}
+                        height={69}
+                        className={"img-fluid"}
+                        alt={"WEEE Open"}
+                    />
+                </Navbar.Brand>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
