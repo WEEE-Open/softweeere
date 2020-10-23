3b6945ab11abeb139b6069cd01bc06c8bc05e10b
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 15:54:01 +0200

    use embedTypes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
+import embedTypes from "./utils";
 import Header from "./components/Header";
 import MainView from "./components/MainView";
 
@@ -13,19 +14,19 @@ const App = () => {
     useEffect(() => {
         setEmbeds([
             {
-                'type': 'python',
+                'type': embedTypes.python,
                 'title': '🐟 S.A.R.D.I.N.A.',
                 'katacoda': 'e-caste/weee-open-sardina',
                 'github': 'weee-open/sardina',
             },
             {
-                'type': 'php',
+                'type': embedTypes.php,
                 'title': '✅ WEEEhire-ng',
                 'katacoda': 'e-caste/weee-open-weeehire-ng',
                 'github': 'weee-open/weeehire-ng',
             },
             {
-                'type': 'python',
+                'type': embedTypes.python,
                 'title': '🍐 P.E.R.A.C.O.T.T.A.',
                 'katacoda': 'e-caste/weee-open-peracotta',
                 'github': 'weee-open/peracotta',
