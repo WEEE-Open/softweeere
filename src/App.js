83a3351d73b478b415952d192cabc371aa4b3f9f
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 19:05:06 +0200

    add isActive property to each embed, add buttonHandler

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {embedTypes} from "./utils";
 import Header from "./components/Header";
@@ -14,6 +14,7 @@ const App = () => {
             'description': 'Statistiche Amabili Rendimento Degli Informatici Nell’Anno',
             'katacoda': 'e-caste/weee-open-sardina',
             'github': 'weee-open/sardina',
+            'isActive': false,
         },
         {
             'type': embedTypes.php,
@@ -21,6 +22,7 @@ const App = () => {
             'description': 'Manage applications to the team',
             'katacoda': 'e-caste/weee-open-weeehire-ng',
             'github': 'weee-open/weeehire-ng',
+            'isActive': false,
         },
         {
             'type': embedTypes.python,
@@ -28,14 +30,29 @@ const App = () => {
             'description': 'Progetto Esteso Raccolta Automatica Configurazioni hardware Organizzate Tramite Tarallo Autonomamente',
             'katacoda': 'e-caste/weee-open-peracotta',
             'github': 'weee-open/peracotta',
+            'isActive': false,
         }
     ]);
     const [color, setColor] = useState("00983a");
 
+    const buttonHandler = embed => setEmbeds(prevState => {
+        return prevState.map(e => {
+            if (e.github === embed.github) {
+                let newE = {...e};  // CRUCIAL STEP
+                newE.isActive = !newE.isActive;
+                return newE;
+            } else return e;
+        });
+    });
+
     return (
         <div className="App">
             <Header />
-            <MainView embeds={embeds} color={color} />
+            <MainView
+                embeds={embeds}
+                buttonHandler={buttonHandler}
+                color={color}
+            />
         </div>
     );
 }
