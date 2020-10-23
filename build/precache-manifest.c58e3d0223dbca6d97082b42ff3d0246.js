9840f3dbbb04e32d996dffeb3be932ae0ce8b4bb
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 11:24:41 +0200

    add build directory for production. close #4

---
+++ build/precache-manifest.c58e3d0223dbca6d97082b42ff3d0246.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "356923f294af606e0ed634d9dea704df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8588566186f43f643df",
+    "url": "/static/css/main.a69a63fb.chunk.css"
+  },
+  {
+    "revision": "5f4ce7d0e0c9ede9f76f",
+    "url": "/static/js/2.ca502407.chunk.js"
+  },
+  {
+    "revision": "6bbb3a8e7360a6a98672414629b231d4",
+    "url": "/static/js/2.ca502407.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8588566186f43f643df",
+    "url": "/static/js/main.ddf1bc80.chunk.js"
+  },
+  {
+    "revision": "99dfb135a191a9dda95f",
+    "url": "/static/js/runtime-main.17528f9d.js"
+  }
+]);
