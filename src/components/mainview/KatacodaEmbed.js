3ffd35a8d817d10001c55cf12b3801184ebea989
e-caste-mbp <en.cas.dev@icloud.com>
2020-10-23 19:07:45 +0200

    remove github link from KatacodaEmbed

--- src/components/mainview/KatacodaEmbed.js
+++ src/components/mainview/KatacodaEmbed.js
@@ -6,8 +6,6 @@ const KatacodaEmbed = props => {
     return (
         <div id={`embed-${num}`} className={"pt-3"}>
             <h3>{embed.title}</h3>
-            <hr/>
-            <p>Find more info on the <a href={`https://github.com/${embed.github}`}>GitHub page</a></p>
             <div id={`katacoda-scenario-${num}`}
                 data-katacoda-id={embed.katacoda}
                 data-katacoda-color={color}
