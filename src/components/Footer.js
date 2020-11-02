2682bebc310fd0ff7ee8f90c310483d80f0a1250
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-02 09:52:33 +0100

    refactor Footer: more concise

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,14 +11,14 @@ const Footer = props => {
                     Contribute or clone this website <a href="https://github.com/e-caste/softweeere" target="_blank" rel="nofollow noopener noreferrer">here</a>!
                 </p>
                 <p className={"flex-row text-center"}>
-                    Follow us on Facebook:
-                    <a href="https://www.facebook.com/weeeopen/" target="_blank" rel="nofollow noopener noreferrer"> Team WEEE Open</a>
-                    , Instagram:
-                    <a href="https://www.instagram.com/weeeopen/" target="_blank" rel="nofollow noopener noreferrer"> @weeeopen</a>
-                    , LinkedIn:
-                    <a href="https://www.linkedin.com/company/weee-open/" target="_blank" rel="nofollow noopener noreferrer"> WEEE Open </a>
-                    and on PoliTOnet:
-                    <a href="https://politecnico-di-torino.hivebrite.com/topics/9837/" target="_blank" rel="nofollow noopener noreferrer"> Team WEEE Open</a>
+                    Follow us on
+                    <a href="https://www.facebook.com/weeeopen/" target="_blank" rel="nofollow noopener noreferrer"> Facebook</a>
+                    ,
+                    <a href="https://www.instagram.com/weeeopen/" target="_blank" rel="nofollow noopener noreferrer"> Instagram</a>
+                    ,
+                    <a href="https://www.linkedin.com/company/weee-open/" target="_blank" rel="nofollow noopener noreferrer"> LinkedIn </a>
+                    and on
+                    <a href="https://politecnico-di-torino.hivebrite.com/topics/9837/" target="_blank" rel="nofollow noopener noreferrer"> PoliTOnet</a>
                 </p>
                 <p>
                     Licensing found <a href={"https://github.com/weee-open/softweeere#licensing"} target="_blank" rel="nofollow noopener noreferrer">here</a>.
