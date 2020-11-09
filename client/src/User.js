1d45e98b91fcea246df9b79af417b20c7ef0cd2a
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-09 19:03:51 +0100

    remove unnecessary User constructor

--- client/src/User.js
+++ client/src/User.js
@@ -1,20 +1,4 @@
 class User {
-    constructor(
-        id,
-        is_active,
-        is_superuser,
-        email,
-        container_ids,
-        expire_unix,
-    ) {
-        this.id = id;
-        this.isActive = is_active;
-        this.isSuperuser = is_superuser;
-        this.email = email;
-        this.containerIds = container_ids;
-        this.expireUnix = expire_unix;
-    }
-
     static from(json) {
         // convert snake_case to camelCase
         json.isActive = json.is_active;
