735754450f993dd771d22c5d435bdc18e574848f
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-09 19:03:32 +0100

    add snake_case to camelCase conversion

--- client/src/User.js
+++ client/src/User.js
@@ -16,6 +16,15 @@ class User {
     }
 
     static from(json) {
+        // convert snake_case to camelCase
+        json.isActive = json.is_active;
+        json.isSuperuser = json.is_superuser;
+        json.containerIds = json.container_ids;
+        json.expireUnix = json.expire_unix;
+        delete json.is_active;
+        delete json.is_superuser;
+        delete json.container_ids;
+        delete json.expire_unix;
         return Object.assign(new User(), json);
     }
 }
