9d6bd7b4cb3b326a06570541faba6ac69d7b80d6
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-10 16:50:07 +0100

    connect xterm to custom websocket

--- client/src/customXterm.js
+++ client/src/customXterm.js
@@ -2,16 +2,16 @@ import {Terminal} from "xterm";
 import "xterm/css/xterm.css";
 import {AttachAddon} from "xterm-addon-attach";
 
-export const mountTerminalAtId = id => {
-    // see https://xtermjs.org/docs/api/addons/attach/
+export const mountTerminal = (id, repo, cnt_id, email) => {
     const term = new Terminal();
     term.setOption("theme", {
         background: "#101010",
         foreground: "#00983a",
     });
-    // const socket = new WebSocket();
-    // const attachAddon = new AttachAddon(socket);
-    // term.loadAddon(attachAddon);
+    // see https://xtermjs.org/docs/api/addons/attach/
+    const socket = new WebSocket(`ws://localhost:3000/${repo}?cnt_id=${cnt_id}?user_email=${email}`);
+    const attachAddon = new AttachAddon(socket);
+    term.loadAddon(attachAddon);
     term.open(document.getElementById(id));
     term.write('Hello from \x1B[1;3;31mxterm.js\x1B[0m $ ');
 }
