d98b2f1148cbdda441d8980b3136b0f1be7f3631
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-10 17:37:36 +0100

    pass parameters to mountTerminal function in DockerTerminal

--- client/src/components/mainview/DockerTerminal.js
+++ client/src/components/mainview/DockerTerminal.js
@@ -1,13 +1,21 @@
 import React, {useEffect} from "react";
-import {mountTerminalAtId} from "../../customXterm";
+import {mountTerminal} from "../../customXterm";
 
 const DockerTerminal = props => {
 
-    const id = "terminal";
+    const {user, repo} = props;
+
+    const id = `terminal-${repo}`;
+    let cnt_id = "";
+    let email = "";
+    if (user.containerIds !== undefined)
+        cnt_id = user.containerIds[repo];
+        email = user.email;
 
     useEffect(() => {
-        mountTerminalAtId(id);
-    }, []);
+        if (cnt_id && email)
+            mountTerminal(id, repo, cnt_id, email);
+    }, [id, repo, cnt_id, email]);
 
     return (
         <div>
