4b9e17a1f5d7176b0aeb474865af1d37ad089e19
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-10 11:33:05 +0100

    add DockerTerminal component

--- client/src/components/mainview/DockerTerminal.js
+++ client/src/components/mainview/DockerTerminal.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, {useEffect} from "react";
+import {mountTerminalAtId} from "../../customXterm";
 
 const DockerTerminal = props => {
 
+    const id = "terminal";
+
+    useEffect(() => {
+        mountTerminalAtId(id);
+    }, []);
+
     return (
         <div>
+            <div id={id} />
         </div>
     );
 }
