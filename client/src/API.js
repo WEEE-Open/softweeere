79811b82974e9d2768ae002873e87f792a181b69
e-caste-mbp <en.cas.dev@icloud.com>
2020-11-12 21:28:36 +0100

    comment out deleteContainer client-side API

--- client/src/API.js
+++ client/src/API.js
@@ -24,20 +24,20 @@ async function getContainer(repo) {
     }));
 }
 
-async function deleteContainer(email, repo, cnt_id) {
-    return new Promise(((resolve, reject) => {
-        fetch(`${apiPrefix}/container/${repo}`, {
-            method: "DELETE",
-            headers: {"Content-Type": "application/json"},
-            body: JSON.stringify({user_email: email, cnt_id: cnt_id})
-        }).then(res => {
-            if (res.ok)
-                resolve(res.json());
-            else
-                reject(res.json().error);
-        }).catch(err => reject(err));
-    }));
-}
+// async function deleteContainer(email, repo, cnt_id) {
+//     return new Promise(((resolve, reject) => {
+//         fetch(`${apiPrefix}/container/${repo}`, {
+//             method: "DELETE",
+//             headers: {"Content-Type": "application/json"},
+//             body: JSON.stringify({user_email: email, cnt_id: cnt_id})
+//         }).then(res => {
+//             if (res.ok)
+//                 resolve(res.json());
+//             else
+//                 reject(res.json().error);
+//         }).catch(err => reject(err));
+//     }));
+// }
 
-const API = {getRepos, getContainer, deleteContainer};
+const API = {getRepos, getContainer};
 export default API;
